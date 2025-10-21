4c0f1f6d699713ea94b115d00e9cf59020f3cd0e
Peter Brown <peter.brown98@hotmail.co.uk>
2025-10-22 00:54:01 +0100

    Added product page with sorting and filtering

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,27 +1,14 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./layout/header/header";
-import { HttpClient } from '@angular/common/http';
-import { Product } from './shared/models/product';
-import { Pagination } from './shared/models/pagination';
+import { ShopComponent } from "./features/shop/shop.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, ShopComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
-  baseUrl = 'https://localhost:5001/api/';
-  private http = inject(HttpClient);
-  protected readonly title = signal('client');
-  products: Product[] = [];
-
-  ngOnInit(): void {
-      this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
-        next: response => this.products = response.data,
-        error: error => console.log(error), 
-        complete: () => console.log('complete')
-      });
-  }
+export class AppComponent {
+  title = 'Skinet';
 }
